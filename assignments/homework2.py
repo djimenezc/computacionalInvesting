f4aa80ad794bbb2b211ad363210e74e965e321a5
david jimenez <david.jimenez19@gmail.com>
2017-06-11 12:06:42 +0200

    homework2: format file

--- assignments/homework2.py
+++ assignments/homework2.py
@@ -1,6 +1,7 @@
 """
 Author: Vitor Monteiro
-Homework 2 (http://wiki.quantsoftware.org/index.php?title=CompInvestI_Homework_2)
+Homework 2
+(http://wiki.quantsoftware.org/index.php?title=CompInvestI_Homework_2)
 """
 
 import copy
@@ -12,41 +13,44 @@ import QSTK.qstkutil.DataAccess as da
 import QSTK.qstkstudy.EventProfiler as ep
 
 
+''' Finding the event dataframe '''
+
+
 def find_events(ls_symbols, d_data):
-    ''' Finding the event dataframe '''
-    dt_actualClose = d_data['actual_close']
+    dt_actual_close = d_data['actual_close']
 
     print "Finding Events"
 
     # Creating an empty dataframe, using df_close just for size
-    df_events = copy.deepcopy(dt_actualClose)
+    df_events = copy.deepcopy(dt_actual_close)
     df_events = df_events * np.NAN
 
     # Time stamps for the event range
-    ldt_timestamps = dt_actualClose.index
+    ldt_timestamps = dt_actual_close.index
 
     for s_sym in ls_symbols:
         for i in range(1, len(ldt_timestamps)):
             # Calculating if the event happens for this day i
-            acToday = dt_actualClose[s_sym].ix[ldt_timestamps[i]]
-            acYesterday = dt_actualClose[s_sym].ix[ldt_timestamps[i - 1]]
+            ac_today = dt_actual_close[s_sym].ix[ldt_timestamps[i]]
+            ac_yesterday = dt_actual_close[s_sym].ix[ldt_timestamps[i - 1]]
 
-            # Event is found if the symbol is was >= 5 yesterday and less than 5 today
-            if acYesterday >= 5.0 > acToday:
+            # Event is found if the symbol is was >= 5 yesterday
+            # and less than 5 today
+            if ac_yesterday >= 5.0 > ac_today:
                 df_events[s_sym].ix[ldt_timestamps[i]] = 1
 
     return df_events
 
 
-def generateEventProfiler(dt_start, dt_end, listName, eventId):
+def generate_event_profiler(dt_start, dt_end, list_name, event_id):
     ldt_timestamps = du.getNYSEdays(dt_start, dt_end, dt.timedelta(hours=16))
 
-    dataobj = da.DataAccess('Yahoo')
-    ls_symbols = dataobj.get_symbols_from_list(listName)
+    data_obj = da.DataAccess('Yahoo')
+    ls_symbols = data_obj.get_symbols_from_list(list_name)
     ls_symbols.append('SPY')
 
     ls_keys = ['open', 'high', 'low', 'close', 'volume', 'actual_close']
-    ldf_data = dataobj.get_data(ldt_timestamps, ls_symbols, ls_keys)
+    ldf_data = data_obj.get_data(ldt_timestamps, ls_symbols, ls_keys)
     d_data = dict(zip(ls_keys, ldf_data))
 
     for s_key in ls_keys:
@@ -55,14 +59,17 @@ def generateEventProfiler(dt_start, dt_end, listName, eventId):
         d_data[s_key] = d_data[s_key].fillna(1.0)
 
     df_events = find_events(ls_symbols, d_data)
-    eventFileName = eventId + ".pdf"
-    print "Creating Study for:", eventId
+    event_file_name = event_id + ".pdf"
+    print "Creating Study for:", event_id
 
     ep.eventprofiler(df_events, d_data, i_lookback=20, i_lookforward=20,
-                     s_filename=eventFileName, b_market_neutral=True, b_errorbars=True,
+                     s_filename=event_file_name, b_market_neutral=True,
+                     b_errorbars=True,
                      s_market_sym='SPY')
 
 
 if __name__ == '__main__':
-    generateEventProfiler(dt.datetime(2008, 1, 1), dt.datetime(2009, 12, 31), "sp5002008", "sp50008-2008-2009")
-    generateEventProfiler(dt.datetime(2008, 1, 1), dt.datetime(2009, 12, 31), "sp5002012", "sp50012-2008-2009")
+    generate_event_profiler(dt.datetime(2008, 1, 1), dt.datetime(2009, 12, 31),
+                            "sp5002008", "sp50008-2008-2009")
+    generate_event_profiler(dt.datetime(2008, 1, 1), dt.datetime(2009, 12, 31),
+                            "sp5002012", "sp50012-2008-2009")
